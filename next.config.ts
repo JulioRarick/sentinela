cadb287bdb4e587d899eee01c29f175d879e3f20
Julio Rarick <juliorarick@gmail.com>
2024-12-28 05:13:28 -0300

    📝 update: next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  basePath: '/sentinela',
+  assetPrefix: '/sentinela/',
+  trailingSlash: true,
+  reactStrictMode: true,
+}
 
 export default nextConfig
