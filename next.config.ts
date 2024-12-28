f351e1f20a8907c88bc695ffe80d6f82533789ad
Julio Rarick <juliorarick@gmail.com>
2024-12-28 18:19:22 -0300

    redeploy

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   basePath: '/sentinela',
-  assetPrefix: '/sentinela/',
-  trailingSlash: true,
-  reactStrictMode: true,
 }
 
 export default nextConfig
